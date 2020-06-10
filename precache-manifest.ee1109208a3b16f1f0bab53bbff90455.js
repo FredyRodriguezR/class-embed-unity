5c752cbf991b7bf1d59571e307279bf2fa9c163b
Fredy <megablastoise94@gmail.com>
2020-06-10 15:15:21 -0500

    Updates

---
+++ precache-manifest.ee1109208a3b16f1f0bab53bbff90455.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f45f5646acfca23a4bdf77b8250336a2",
+    "url": "/class-embed-unity/index.html"
+  },
+  {
+    "revision": "dfff75fa3e75e7c2c9dc",
+    "url": "/class-embed-unity/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "effa886a6e37b2cdd020",
+    "url": "/class-embed-unity/static/js/2.d05d3d61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/class-embed-unity/static/js/2.d05d3d61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfff75fa3e75e7c2c9dc",
+    "url": "/class-embed-unity/static/js/main.c291ee42.chunk.js"
+  },
+  {
+    "revision": "2ab06d0d8011d23fd96f",
+    "url": "/class-embed-unity/static/js/runtime-main.bb4fff57.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/class-embed-unity/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/class-embed-unity/precache-manifest.ee1109208a3b16f1f0bab53bbff90455.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/class-embed-unity/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
